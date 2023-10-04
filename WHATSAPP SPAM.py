77c988bd27c8fd3c5c24a31bf3becd13a016b17e
hallowshaw <rhitam.rit54@gmail.com>
2023-10-05 01:01:47 +0530

    first commit

---
+++ WHATSAPP SPAM.py
@@ -0,0 +1,17 @@
+import pyautogui as pt
+import time
+
+print("After putting the limit and message, open the chatbox of the user you want to spam to")
+
+limit = input("\nEnter limit:")
+
+message = input("\nEnter message:")
+i = 0
+time.sleep(5)
+
+while i < int(limit):
+    pt.typewrite(message)
+        
+    pt.press("enter")
+
+    i+=1
